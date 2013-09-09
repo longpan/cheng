5db7f970c133acb01ff4c5cc1f90e3fc86c04e2d
longpan <619692439@qq.com>
2013-09-09 08:56:21 +0800

    修改
    visualstudio 2012编译的

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,6 +1,11 @@
-#include<stdio.h>
-main()
-{
-	printf("hello,world\n");
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+#include"stdafx.h"
+#include<iostream>
+using namespace std;
 
+int main()
+{
+	cout << "helloworld!ÎÒÊÇÁúÃ¯¾¸£¡" << endl;
+	return 0;
 }
